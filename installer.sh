c752e744d899868726f72a889bb965fba6f88707
Ali Yasir <131984413+ali-yasir-binairy@users.noreply.github.com>
2024-01-03 16:25:04 +0500

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -20,124 +20,7 @@ spinner() {
     printf "      \b\b\b\b\b"
 }
 
-# Check for internet connection
-if ! ping -c 1 8.8.8.8 &>/dev/null; then
-    echo "No internet connection. Please check your connection and try again."
-    exit 1
-fi
-
-# Check if TOKEN environment variable is set and not empty
-if [[ -z "${TOKEN}" ]]; then
-    echo "Please set GitHub Auth token and run again."
-    exit 1
-fi
-
-# Base directory
-BASE_DIR=~/NFCEmu
-
-# Check if it's a fresh install or an update
-if [[ -d "${BASE_DIR}/NFC-TerminalGUI-main" && -d "${BASE_DIR}/NFCEmulator-1-main" ]]; then
-    UPDATE=true
-else
-    UPDATE=false
-    # Update the package list
-    sudo apt-get update
-
-    # Upgrade installed packages
-    sudo apt-get upgrade -y
-
-    # Install necessary packages
-    sudo apt-get install -y git autoconf libtool libusb-dev
-
-    # Enable SPI interface using raspi-config
-    sudo raspi-config nonint do_spi 0
-
-    # Clone the libnfc repository
-    cd ~
-    git clone https://github.com/nfc-tools/libnfc
-
-    # Navigate to the libnfc directory
-    cd libnfc
-
-    # Create the /etc/nfc directory if it doesn't exist
-    sudo mkdir -p /etc/nfc
-
-    file_path="/etc/nfc/libnfc.conf"
-    text_to_add="allow_autoscan = true
-    device.name = \"PN532 over SPI\"
-    device.connstring = \"pn532_spi:/dev/spidev0.0:100000\""
-    
-    # Check if the file contains the text
-    if ! grep -q "$text_to_add" "$file_path"; then
-        # If not found, append the text to the file
-        echo "$text_to_add" | sudo tee -a "$file_path" > /dev/null
-        echo "Text added to $file_path."
-    else
-        echo "Text already present in $file_path."
-    fi
-
-    # Run additional setup commands
-    autoreconf -vis
-    ./configure --with-drivers=pn532_spi --sysconfdir=/etc --prefix=/usr
-    make
-    sudo make install all
-fi
-
 # Display messages in big font
-display_installing_nfc_terminal() {
-    orange=$(tput setaf 3)  # ANSI code for orange
-    # ASCII art for the banner
-    banner="
-    
-    ${orange}
-    ██╗███╗   ██╗███████╗████████╗ █████╗ ██╗     ██╗     ██╗███╗   ██╗ ██████╗                    
-    ██║████╗  ██║██╔════╝╚══██╔══╝██╔══██╗██║     ██║     ██║████╗  ██║██╔════╝                    
-    ██║██╔██╗ ██║███████╗   ██║   ███████║██║     ██║     ██║██╔██╗ ██║██║  ███╗                   
-    ██║██║╚██╗██║╚════██║   ██║   ██╔══██║██║     ██║     ██║██║╚██╗██║██║   ██║                   
-    ██║██║ ╚████║███████║   ██║   ██║  ██║███████╗███████╗██║██║ ╚████║╚██████╔╝                   
-    ╚═╝╚═╝  ╚═══╝╚══════╝   ╚═╝   ╚═╝  ╚═╝╚══════╝╚══════╝╚═╝╚═╝  ╚═══╝ ╚═════╝                    
-                                                                                                   
-    ███╗   ██╗███████╗ ██████╗    ████████╗███████╗██████╗ ███╗   ███╗██╗███╗   ██╗ █████╗ ██╗     
-    ████╗  ██║██╔════╝██╔════╝    ╚══██╔══╝██╔════╝██╔══██╗████╗ ████║██║████╗  ██║██╔══██╗██║     
-    ██╔██╗ ██║█████╗  ██║            ██║   █████╗  ██████╔╝██╔████╔██║██║██╔██╗ ██║███████║██║     
-    ██║╚██╗██║██╔══╝  ██║            ██║   ██╔══╝  ██╔══██╗██║╚██╔╝██║██║██║╚██╗██║██╔══██║██║     
-    ██║ ╚████║██║     ╚██████╗       ██║   ███████╗██║  ██║██║ ╚═╝ ██║██║██║ ╚████║██║  ██║███████╗
-    ╚═╝  ╚═══╝╚═╝      ╚═════╝       ╚═╝   ╚══════╝╚═╝  ╚═╝╚═╝     ╚═╝╚═╝╚═╝  ╚═══╝╚═╝  ╚═╝╚══════╝
-                                      
-                                                                                                                                                                                                                                                                                                                                                          
-    "
-
-    # Print the banner
-    echo -e "${banner}"
-}
-
-display_updating_nfc_terminal() {
-    orange=$(tput setaf 3)  # ANSI code for orange
-    # ASCII art for the banner
-    banner="
-    
-    ${orange}
-    ██╗   ██╗██████╗ ██████╗  █████╗ ████████╗██╗███╗   ██╗ ██████╗                                
-    ██║   ██║██╔══██╗██╔══██╗██╔══██╗╚══██╔══╝██║████╗  ██║██╔════╝                                
-    ██║   ██║██████╔╝██║  ██║███████║   ██║   ██║██╔██╗ ██║██║  ███╗                               
-    ██║   ██║██╔═══╝ ██║  ██║██╔══██║   ██║   ██║██║╚██╗██║██║   ██║                               
-    ╚██████╔╝██║     ██████╔╝██║  ██║   ██║   ██║██║ ╚████║╚██████╔╝                               
-     ╚═════╝ ╚═╝     ╚═════╝ ╚═╝  ╚═╝   ╚═╝   ╚═╝╚═╝  ╚═══╝ ╚═════╝                                
-                                                                                                   
-    ███╗   ██╗███████╗ ██████╗    ████████╗███████╗██████╗ ███╗   ███╗██╗███╗   ██╗ █████╗ ██╗     
-    ████╗  ██║██╔════╝██╔════╝    ╚══██╔══╝██╔════╝██╔══██╗████╗ ████║██║████╗  ██║██╔══██╗██║     
-    ██╔██╗ ██║█████╗  ██║            ██║   █████╗  ██████╔╝██╔████╔██║██║██╔██╗ ██║███████║██║     
-    ██║╚██╗██║██╔══╝  ██║            ██║   ██╔══╝  ██╔══██╗██║╚██╔╝██║██║██║╚██╗██║██╔══██║██║     
-    ██║ ╚████║██║     ╚██████╗       ██║   ███████╗██║  ██║██║ ╚═╝ ██║██║██║ ╚████║██║  ██║███████╗
-    ╚═╝  ╚═══╝╚═╝      ╚═════╝       ╚═╝   ╚══════╝╚═╝  ╚═╝╚═╝     ╚═╝╚═╝╚═╝  ╚═══╝╚═╝  ╚═╝╚══════╝
-                                                                                               
-                                                                                                                                                                                                                                                                                                                                               
-    "
-
-    # Print the banner
-    echo -e "${banner}"
-}
-
 display_message() {
     local msg="$1"
     local box_top="╭"
@@ -158,11 +41,97 @@ display_message() {
     echo -e "\n"
 }
 
+log_info() {
+    local msg="$1"
+    blue=$(tput setaf 4)  # ANSI code for orange
+    reset=$(tput sgr0)
+
+    echo -e "\n"
+    printf "{$blue}⫸ %s{$reset}\n" "$msg"
+}
+
+log_error() {
+    local msg="$1"
+    red=$(tput setaf 1)  # ANSI code for orange
+    reset=$(tput sgr0)
+
+    echo -e "\n"
+    printf "{$red}⫸ %s{$reset}\n" "$msg"
+}
+
+# Check for internet connection
+if ! ping -c 1 8.8.8.8 &>/dev/null; then
+    log_error "No internet connection. Please check your connection and try again."
+    exit 1
+fi
+
+# Check if TOKEN environment variable is set and not empty
+if [[ -z "${TOKEN}" ]]; then
+    log_error "Please set GitHub Auth token and run again."
+    exit 1
+fi
+
+# Base directory
+BASE_DIR=~/NFCEmu
+
+# Check if it's a fresh install or an update
+if [[ -d "${BASE_DIR}/NFC-TerminalGUI-main" && -d "${BASE_DIR}/NFCEmulator-1-main" ]]; then
+    log_info "Directories already exist. Updating them."
+    UPDATE=true 
+else
+    log_info "Performing a Fresh Install."
+    UPDATE=false
+    # Update the package list
+    sudo apt-get update
+
+    # Upgrade installed packages
+    sudo apt-get upgrade -y
+
+    # Install necessary packages
+    sudo apt-get install -y git autoconf libtool libusb-dev
+
+    # Enable SPI interface using raspi-config
+    log_info "Enabling the SPI interface on Pi..."
+    sudo raspi-config nonint do_spi 0
+
+    # Clone the libnfc repository
+    log_info "Cloning the libnfc repo..."
+    cd ~
+    git clone https://github.com/nfc-tools/libnfc
+
+    # Navigate to the libnfc directory
+    cd libnfc
+
+    # Create the /etc/nfc directory if it doesn't exist
+    sudo mkdir -p /etc/nfc
+
+    log_info "Adding NFC configuration to libnfc.conf..."
+    file_path="/etc/nfc/libnfc.conf"
+    text_to_add="allow_autoscan = true
+    device.name = \"PN532 over SPI\"
+    device.connstring = \"pn532_spi:/dev/spidev0.0:100000\""
+    
+    # Check if the file contains the text
+    if ! grep -q "$text_to_add" "$file_path"; then
+        # If not found, append the text to the file
+        echo "$text_to_add" | sudo tee -a "$file_path" > /dev/null
+        log_info "Config added to $file_path."
+    else
+        log_info "Config already exists. Not added."
+    fi
+
+    # Run additional setup commands
+    autoreconf -vis
+    ./configure --with-drivers=pn532_spi --sysconfdir=/etc --prefix=/usr
+    make
+    sudo make install all
+fi
+
 # Start message
 if [ "$UPDATE" = true ]; then
-    display_updating_nfc_terminal
+    display_message "UPDATING NFC TERMINAL APP"
 else
-    display_installing_nfc_terminal
+    display_message "INSTAllING NFC TERMINAL APP"
 fi
 
 # Create the base directory if it doesn't exist
@@ -175,6 +144,7 @@ download_and_extract() {
     local output_zip="${BASE_DIR}/${folder_name}.zip"
 
     # Download the repository
+    log_info "Downloading the ${folder_name} repo..."
     curl -s -H "Authorization: token ${TOKEN}" -L "${repo_url}" -o "${output_zip}" &
 
     # Start the spinner animation
@@ -184,6 +154,7 @@ download_and_extract() {
     wait
 
     # Extract the repository
+    log_info "Extracting the ${folder_name}.zip..."
     unzip -o -q "${output_zip}" -d "${BASE_DIR}"
     # rm "${output_zip}"  # Remove the downloaded zip file
 }
@@ -213,27 +184,32 @@ cd "${BASE_DIR}/NFCEmulator-1-main" || exit
 rm -rf !("Firmware")
 cd Firmware || exit
 rm -rf !("RPi_AndroidHCE")
+
 cd RPi_AndroidHCE || exit
+log_info "Making the android_hce script..."
 make all & 
 spinner $! &
 wait
+log_info "android_hce script make completed."
+
 
 # Get run script
 cd ${BASE_DIR} || exit
 wget https://raw.githubusercontent.com/Osman-Ashraf/NFCEmu-QuickStart/ali-yasir-binairy-patch-1/run.sh -O ${BASE_DIR}/run.sh 
 wait
+log_info "Making the run script..."
 chmod +x run.sh
-
-# Make android_hce.sh
-# cd "${BASE_DIR}/NFCEmulator-1-main/Firmware/RPi_AndroidHCE" || exit
-# gcc -o android_hce android_hce.cpp -lnfc
+log_info "run script make completed."
 
 # End message
 if [ "$UPDATE" = true ]; then
     display_message "NFCEmulator Updated"
-    
+
+    log_info "Removing unnecessary zip files in ${BASE_DIR}"
     rm -rf $BASE_DIR/*.zip
 else
     display_message "NFCEmulator Installed"
+
+    log_info "Removing unnecessary zip files in ${BASE_DIR}"
     rm -rf $BASE_DIR/*.zip
 fi
