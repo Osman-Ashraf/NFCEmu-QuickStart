d06f4335972cf52d80a7a6aef1dd92407c8929b5
Ali Yasir <131984413+ali-yasir-binairy@users.noreply.github.com>
2024-04-30 17:45:41 +0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -87,7 +87,8 @@ echo "Starting socket server..."
 
 # Change to GUI directory to ensure relative paths in the Python script work correctly
 cd "$GUI_DIR"
-export DISPLAY=:0.0
+
+# export DISPLAY=:0.0
 python3 "$GUI_PATH" &
 # python3 "$PIR_PATH" &
 
